7bd7f4a369eb27fae00e7cd62acadb5879f14e7f
Suvanjan Mukherjee <suvanjanmukherjee@gmail.com>
2025-09-19 21:00:33 -0400

    Add reverse ssh tunnel server

--- bin/server/server.go
+++ bin/server/server.go
@@ -4,14 +4,22 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net"
+	"os"
 
 	pb "github.com/smukherj1/windows-agent/grpc/server"
+	"golang.org/x/crypto/ssh"
 	"google.golang.org/grpc"
 )
 
-var port = flag.Int("port", 8000, "The server port")
+var (
+	port         = flag.Int("port", 8000, "The server port")
+	remoteServer = flag.String("remote.server", "", "The address of the remote ssh server.")
+	remoteUser   = flag.String("remote.user", "", "The user on the remote ssh server.")
+	privateKey   = flag.String("private.key", "", "The path to the private key for the remote ssh server.")
+)
 
 type server struct {
 	pb.UnimplementedServiceServer
@@ -22,8 +30,76 @@ func (s *server) Hello(_ context.Context, in *pb.HelloRequest) (*pb.HelloReply, 
 	return &pb.HelloReply{Message: "Hello " + in.GetMessage()}, nil
 }
 
+func startReverseSshTunnel(port int, remoteServer, remoteUser, privateKey string) {
+	key, err := os.ReadFile(privateKey)
+	if err != nil {
+		log.Fatalf("unable to read private key: %v", err)
+	}
+
+	// Create the Signer for this private key.
+	signer, err := ssh.ParsePrivateKey(key)
+	if err != nil {
+		log.Fatalf("unable to parse private key: %v", err)
+	}
+
+	config := &ssh.ClientConfig{
+		User: remoteUser,
+		Auth: []ssh.AuthMethod{
+			ssh.PublicKeys(signer),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+
+	// Connect to the remote SSH server.
+	client, err := ssh.Dial("tcp", remoteServer, config)
+	if err != nil {
+		log.Fatalf("unable to connect to remote server: %v", err)
+	}
+	defer client.Close()
+
+	// Listen on the remote server.
+	remotePort := fmt.Sprintf(":%d", port)
+	l, err := client.Listen("tcp", remotePort)
+	if err != nil {
+		log.Fatalf("unable to listen on remote server: %v", err)
+	}
+	defer l.Close()
+
+	localPort := fmt.Sprintf("localhost:%d", port)
+	for {
+		remote, err := l.Accept()
+		if err != nil {
+			log.Printf("failed to accept remote connection: %v", err)
+			continue
+		}
+
+		local, err := net.Dial("tcp", localPort)
+		if err != nil {
+			log.Printf("failed to dial local server: %v", err)
+			continue
+		}
+
+		go func() {
+			defer local.Close()
+			defer remote.Close()
+			io.Copy(local, remote)
+		}()
+
+		go func() {
+			defer local.Close()
+			defer remote.Close()
+			io.Copy(remote, local)
+		}()
+	}
+}
+
 func main() {
 	flag.Parse()
+
+	if *remoteServer != "" {
+		go startReverseSshTunnel(*port, *remoteServer, *remoteUser, *privateKey)
+	}
+
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
--- go.mod
+++ go.mod
@@ -8,8 +8,9 @@ require (
 )
 
 require (
-	golang.org/x/net v0.41.0 // indirect
-	golang.org/x/sys v0.33.0 // indirect
-	golang.org/x/text v0.26.0 // indirect
+	golang.org/x/crypto v0.42.0 // indirect
+	golang.org/x/net v0.43.0 // indirect
+	golang.org/x/sys v0.36.0 // indirect
+	golang.org/x/text v0.29.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250707201910-8d1bb00bc6a7 // indirect
 )
